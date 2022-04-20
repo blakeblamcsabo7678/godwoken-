22a8e96351a17fab4c3d54c1940f5409b2d11ec4
anh cảm ơn <104052580+blakeblamcsabo7678@users.noreply.github.com>
2022-04-20 12:06:03 +0700

    Create fetch-binaries.sh

---
+++ devtools/fetch-binaries.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir_name='.tmp/binaries'
+image='ghcr.io/jjyr/godwoken-prebuilds:refactor-registry-address-202204061147'
+
+docker pull $image
+[ -d $dir_name ] && rm -rf $dir_name && echo "Delete old dir"
+mkdir -p $dir_name && echo "Create dir"
+docker run --rm -v $(pwd)/$dir_name:/tmp $image cp -r /scripts/godwoken-scripts /tmp && echo "Copy scripts"
+docker run --rm -v $(pwd)/$dir_name:/tmp $image cp -r /scripts/godwoken-polyjuice /tmp && echo "Copy polyjuice"
+echo "Done"
